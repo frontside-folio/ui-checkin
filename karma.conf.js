50f7cd03a4077aa3e0f9a2e79532dab0f8978bbc
Eric Valuk <evaluk@ebsco.com>
2018-09-18 15:56:17 -0400

    running karma but test is not finished

--- karma.conf.js
+++ karma.conf.js
@@ -1,27 +1,30 @@
+var path = require('path')
 module.exports = (config) => {
   const testIndex = './test/pact-testing/pact-testing.spec.js';
   const preprocessors = {};
   preprocessors[`${testIndex}`] = ['webpack'];
 
   const configuration = {
-    frameworks:['mocha',"chai",'pact'],
+    frameworks:['mocha', 'pact'],
     plugins: [
       'karma-*',
       '@pact-foundation/karma-pact'
     ],
     files: [
       'node_modules/@pact-foundation/pact-web/pact-web.js',
+      './test/pact-testing/pact-testing.js',
       './test/pact-testing/pact-testing.spec.js',
+      
     ],
 
     preprocessors,
     pact:[{
       consumer: 'ui-checkin',
       provider: 'mod-circulation',
-      port: API_PORT,
+      port: 9130,
       log: path.resolve(process.cwd(), 'logs', 'pact.log'),
       dir: path.resolve(process.cwd(), 'pacts'),
-      logLevel: LOG_LEVEL,
+      logLevel: 'DEBUG',
       spec: 3
     }]
   };
--- test/pact-testing/pact-testing.spec.js
+++ test/pact-testing/pact-testing.spec.js
@@ -1,70 +1,68 @@
 
-const objectUnderTest = require("../../src/Scan.js")
+const pact_testing = require("./pact-testing.js")
 import { beforeEach, describe, it } from '@bigtest/mocha';
 import { expect } from 'chai';
 
-import { describeApplication } from '../bigtest/helpers/describe-application';
+//import { describeApplication } from '../bigtest/helpers/describe-application';
 
 
 
 
-  const barcode =  "9676761472500";
-  const loanId = "asdasdasdasd";
-  const expectedLoanBody = {
-    id : "asdasdfasdf",
-    userid : "erqwerwe",
-    item : {
-      instanceid : "ryutyuityui"
-    }, 
-    systemReturnDate : "enter date here"
-  }
-describeApplication( 'Pact Checkin', () => {
-  const item;
-  var  provider
+const barcode =  "9676761472500";
+const loanId = "asdasdasdasd";
+const expectedLoanBody = {
+  id : "asdasdfasdf",
+  userid : "erqwerwe",
+  item : {
+    instanceid : "ryutyuityui"
+  }, 
+  systemReturnDate : "enter date here"
+}
+describe( 'Pact Checkin', () => {
+  var provider; 
+  
 
   before(function (done) {
-    client = example.createClient('http://localhost:1234')
+    console.log("before fired");
+    
     provider = new Pact.PactWeb({
-      consumer: 'Karma Mocha',
-      provider: 'Hello'
+      consumer: 'ui-checkin',
+      provider: 'mod-circulation'
     })
     // required for slower Travis CI environment
     setTimeout(function () {
       done()
     }, 1000)
+    console.log("provider interaction fired");
+    provider.addInteraction({
+         uponReceiving: 'a request for JSON data',
+         withRequest: {
+           method: 'GET',
+           path: '/circulation/loans',
+           query: {
+             query: "id==" + loanId
+           }
+         },
+         willRespondWith: {
+           status: 200,
+           headers: {
+             'Content-Type': 'application/json; charset=utf-8'
+           },
+           body: expectedLoanBody
+         }
+       })
   })
 
-    beforeEach(() => {
-    
-        return provider.setup()
-          .then(() => {
-            provider.addInteraction({
-              uponReceiving: 'a request for JSON data',
-              withRequest: {
-                method: 'GET',
-                path: '/circulation/loans',
-                query: {
-                  query: "id==" + loanId
-                }
-              },
-              willRespondWith: {
-                status: 200,
-                headers: {
-                  'Content-Type': 'application/json; charset=utf-8'
-                },
-                body: expectedLoanBody
-              }
-            })
-          })
-      })
-      
-
     it("can retrieve the loan by id", done => {
-     const response =  objectUnderTest.fetchLoanById(loanId);
-      
+      console.log("test started " );
+      const response = pact_testing.test()
+      console.log("request recieved");
       expect(response).to.eventually.have.property('');
+      console.log("test over");
+      provider.verify();
     })
     after(() => {
+      console.log("after triggered");
       return provider.finalize()
     })
 })
